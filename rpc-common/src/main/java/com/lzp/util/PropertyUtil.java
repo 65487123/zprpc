b5056336cc5cf307c004148302449aac9b7e2ce5
Lu Zeping <zeping.lu@utstar.com>
2021-01-26 10:14:34 +0800

    STYLE

--- rpc-common/src/main/java/com/lzp/util/PropertyUtil.java
+++ rpc-common/src/main/java/com/lzp/util/PropertyUtil.java
@@ -15,44 +15,43 @@
 
 package com.lzp.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+ import org.slf4j.Logger;
+ import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+ import java.io.InputStream;
+ import java.util.Properties;
 
-/**
- * Description:获取配置文件里的属性
- *
- * @author: Lu ZePing
- * @date: 2020/9/28 10:44
- */
-public class PropertyUtil {
-    private static final Logger logger = LoggerFactory.getLogger(PropertyUtil.class);
-    private static Properties properties = new Properties();
+ /**
+  * Description:获取配置文件里的属性
+  *
+  * @author: Lu ZePing
+  * @date: 2020/9/28 10:44
+  */
+ public class PropertyUtil {
+     private static final Logger logger = LoggerFactory.getLogger(PropertyUtil.class);
+     private static Properties properties = new Properties();
 
-    static {
-        InputStream in = PropertyUtil.class.getClassLoader().getResourceAsStream("zprpc.properties");
-        try {
-            properties.load(in);
-        } catch (IOException e) {
-            logger.error("zprpc.properties was not found", e);
-        }
-    }
-
-
-    public static String getBasePack() {
-        return properties.getProperty("basePack");
-    }
-
-    public static String getConnetionPoolSize() {
-        return properties.getProperty("connetionPoolSize");
-    }
+     static {
+         InputStream in = PropertyUtil.class.getClassLoader().getResourceAsStream("zprpc.properties");
+         try {
+             properties.load(in);
+         } catch (Exception e) {
+             logger.error("load utrpc.properties failed", e)
+         }
+     }
 
 
-    public static String getNacosIpList() {
-        return properties.getProperty("nacosIpList");
-    }
+     public static String getBasePack() {
+         return properties.getProperty("basePack");
+     }
 
-}
+     public static String getConnetionPoolSize() {
+         return properties.getProperty("connetionPoolSize");
+     }
+
+
+     public static String getNacosIpList() {
+         return properties.getProperty("nacosIpList");
+     }
+
+ }
