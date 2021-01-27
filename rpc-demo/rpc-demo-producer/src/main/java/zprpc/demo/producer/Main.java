d4111aca08b39e14ba63f5a81ca35ef212e1a626
Lu Zeping <zeping.lu@utstar.com>
2021-01-27 13:29:34 +0800

    fix documentation

--- rpc-demo/rpc-demo-producer/src/main/java/zprpc/demo/producer/Main.java
+++ rpc-demo/rpc-demo-producer/src/main/java/zprpc/demo/producer/Main.java
@@ -28,8 +28,8 @@ import java.util.concurrent.CountDownLatch;
  * @author zeping lu
  */
 @SpringBootApplication
-//把SpringUtil这个类导入到spring容器中，这样在初始化服务的时候，如果spring容器中有实例，就会用spring容器中的实例
-@Import(SpringUtil.class)
+///把SpringUtil这个类导入到spring容器中，这样在初始化服务的时候，如果spring容器中有实例，就会用spring容器中的实例
+//@Import(SpringUtil.class)
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         ///如果需要用到spring，就加入下面这行，这里不需要
