4dae83d7bd744d6d3f885feb9763962b8e79a48b
Lu Zeping <zeping.lu@utstar.com>
2021-03-27 19:48:35 +0800

    optimize

--- rpc-common/src/main/java/com/lzp/zprpc/common/constant/Cons.java
+++ rpc-common/src/main/java/com/lzp/zprpc/common/constant/Cons.java
@@ -26,7 +26,7 @@ package com.lzp.zprpc.common.constant;
  */
 public class Cons {
     public static final String TIMEOUT = "timeout";
-    public static final String EXCEPTION = "exceptionÈ";
+    public static final String EXCEPTION = "exÈ";
     public static final int TEN = 10;
     public static final int MIN_PORT = 1024;
     public static final int MAX_PORT = 49152;
